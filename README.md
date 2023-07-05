7d27dccec350641ed26a9bcd8e40c65cbf0ee277
inho <inho.choi@mincoding.co.kr>
2023-07-05 14:24:21 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## PrimeFactor
+---
+TDD KATA
---
+++ Sample-Test1/prime-factors_test.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "../Project132/prime-factors.cpp"
+
+TEST(TestCaseName, TestName) {
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
